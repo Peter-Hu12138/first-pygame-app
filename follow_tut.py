ec8f3d84dc3e47755497f08cc4d8fdf9b7a0757d
jthu <jthu@jthus-Mac-mini.local>
2025-02-26 19:41:46 -0500

    updated game over page

--- follow_tut.py
+++ follow_tut.py
@@ -2,6 +2,19 @@ import pygame
 import random
 from sys import exit
 
+def get_curr_time():
+    return pygame.time.get_ticks() - start_time
+
+def get_curr_score():
+    return int(get_curr_time() / 500)
+
+def display_text_at(screen_displaying, text, font, x, y):
+    score_surface = font.render(text, 1, "cyan")
+    score_rect = score_surface.get_rect(center=(x, y))
+    pygame.draw.rect(screen_displaying, "pink", score_rect)
+    pygame.draw.rect(screen_displaying, "pink", score_rect, width=10)
+    screen_displaying.blit(score_surface, score_rect)
+
 WIDTH = 800
 HEIGHT = 400
 
@@ -10,6 +23,7 @@ main_screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Jingtian Hu's REAL first pygame project!")
 clock = pygame.time.Clock()
 game_font = pygame.font.Font("./font/Pixeltype.ttf", 50)
+start_time = 0
 
 # more init
 game_state = "ONG"
@@ -23,21 +37,20 @@ sky_surface = pygame.image.load("./graphics/Sky.png").convert_alpha()
 ground_surface = pygame.image.load("./graphics/ground.png").convert_alpha()
 ground_rect = ground_surface.get_rect(topleft=(0, 300))
 
-score = 0
-text_surface = game_font.render(f"score: {score}", 1, "cyan")
-text_rect = text_surface.get_rect(center=(WIDTH // 2, 50))
-
 over_text_surf = game_font.render("OVER - press esc to start over", 1, "cyan")
-over_text_rect = over_text_surf.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+over_text_rect = over_text_surf.get_rect(center=(WIDTH // 2, HEIGHT // 5))
 
 snail = pygame.image.load("./graphics/snail/snail1.png").convert_alpha()
 snail_rect = snail.get_rect(bottomleft=(800, 300))
 snail_speed = 1
 snail_respawning_prob = 0.01
 
+player_stand_surf = pygame.transform.rotozoom(pygame.image.load("./graphics/Player/player_stand.png"), 0, 2).convert_alpha()
 player_surface = pygame.image.load("./graphics/Player/player_walk_1.png").convert_alpha()
 player_rect = player_surface.get_rect(bottomleft=(100, 300))
 player_vy = 0
+JUMP_V = 15
+GRAVITY_CONSTANT = 0.8
 
 while True:
     for event in pygame.event.get():
@@ -47,21 +60,20 @@ while True:
         if game_state == "ONG":
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if player_rect.collidepoint(event.pos) and event.button == pygame.BUTTON_LEFT:
-                    player_vy -= 10
-                    print("event type collision")
+                    player_vy -= JUMP_V
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and player_rect.bottom == 300:
-                    print("jump!")
-                    player_vy -= 8
+                    player_vy -= JUMP_V
 
         elif game_state == "OVR":
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 game_state = "ONG"
-                # print("state changing to ongoing")
+                # reset game
                 snail_rect = snail.get_rect(bottomleft=(800, 300))
                 player_rect = player_surface.get_rect(bottomleft=(100, 300))
                 player_vy = 0
+                start_time = pygame.time.get_ticks()
 
     if game_state == "ONG":
         
@@ -71,15 +83,9 @@ while True:
         main_screen.blit(sky_surface, (0, 0))
         main_screen.blit(ground_surface, ground_rect)
 
-        text_surface = game_font.render(f"score: {score}", 1, "cyan")
-        text_rect = text_surface.get_rect(center=(WIDTH // 2, 50))
-        pygame.draw.rect(main_screen, "pink", text_rect)
-        pygame.draw.rect(main_screen, "pink", text_rect, width=10)
-        score += 3
-        main_screen.blit(text_surface, text_rect)
+        display_text_at(main_screen, f"score: {get_curr_score()}", game_font, WIDTH // 2, 50)
         main_screen.blit(snail, snail_rect)
         main_screen.blit(player_surface, player_rect)
-
         
         if snail_rect.right <= 0: # replace snail if out frame
             snail_rect.left = 800
@@ -97,23 +103,23 @@ while True:
             player_rect.left -= 4
 
         if player_rect.bottom < 300:
-            player_vy += 0.3
-        else:
-            if player_vy > 0:
+            player_vy += GRAVITY_CONSTANT
+        else: # readjust the position to be on the ground
+            if player_vy > 0: # on the ground falling results in a stop
                 player_vy = 0
+                player_rect.bottom = 300
 
-        if player_rect.colliderect(ground_rect):
-            print("player colliding with ground, adjusting")
-            player_rect.bottom = 300
 
         if player_rect.colliderect(snail_rect):
             game_state = "OVR"
-            score = 0
+            score = get_curr_score()
 
         player_rect.bottom += player_vy
     elif game_state == "OVR":
         main_screen.fill("pink")
-        main_screen.blit(over_text_surf, over_text_rect)        
+        main_screen.blit(player_stand_surf, player_stand_surf.get_rect(center=(WIDTH // 2, HEIGHT // 2)))
+        display_text_at(main_screen, f"score: {score}", game_font, WIDTH // 2, HEIGHT * 4 // 5)
+        main_screen.blit(over_text_surf, over_text_rect)
     
 
 
--- random_drawing.py
+++ random_drawing.py
@@ -2,8 +2,8 @@ import pygame
 import random
 from sys import exit
 
-WIDTH = 800
-HEIGHT = 800
+WIDTH = 400
+HEIGHT = 400
 
 pygame.init()
 main_screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -27,36 +27,36 @@ while True:
             pygame.quit()
             exit()
     
-    for x in range(WIDTH):
-        for y in range(HEIGHT):
-            num_of_pixels = 0
-            r_total = 0
-            g_total = 0
-            b_total = 0
-            for d_x in [-1, 0, 1]:
-                for d_y in [-1, 0, 1]:
-                    try:
-                        color = main_screen.get_at((x + d_x, y + d_y))
-                        num_of_pixels += 1
-                    except:
-                        color = pygame.Color(0, 0, 0)
-                    finally:
-                        r_total += color.r
-                        g_total += color.g
-                        b_total += color.b
-            next_clr_matrix[x][y] = pygame.Color(int(r_total / num_of_pixels), int(g_total / num_of_pixels), int(b_total / num_of_pixels))
-    
-    for x in range(WIDTH):
-        for y in range(HEIGHT):
-            main_screen.set_at((x, y), next_clr_matrix[x][y])
-
     # for x in range(WIDTH):
     #     for y in range(HEIGHT):
-    #         r = int(random.random() * (255 + 1))
-    #         g = int(random.random() * (255 + 1))
-    #         b = int(random.random() * (255 + 1))
-    #         color = pygame.Color(r, g, b)
-    #         main_screen.set_at((x, y), color)
+    #         num_of_pixels = 0
+    #         r_total = 0
+    #         g_total = 0
+    #         b_total = 0
+    #         for d_x in [-1, 0, 1]:
+    #             for d_y in [-1, 0, 1]:
+    #                 try:
+    #                     color = main_screen.get_at((x + d_x, y + d_y))
+    #                     num_of_pixels += 1
+    #                 except:
+    #                     color = pygame.Color(0, 0, 0)
+    #                 finally:
+    #                     r_total += color.r
+    #                     g_total += color.g
+    #                     b_total += color.b
+    #         next_clr_matrix[x][y] = pygame.Color(int(r_total / num_of_pixels), int(g_total / num_of_pixels), int(b_total / num_of_pixels))
+    
+    # for x in range(WIDTH):
+    #     for y in range(HEIGHT):
+    #         main_screen.set_at((x, y), next_clr_matrix[x][y])
+
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            r = int(random.random() * (255 + 1))
+            g = int(random.random() * (255 + 1))
+            b = int(random.random() * (255 + 1))
+            color = pygame.Color(r, g, b)
+            main_screen.set_at((x, y), color)
 
     # actions
     pygame.display.update()
